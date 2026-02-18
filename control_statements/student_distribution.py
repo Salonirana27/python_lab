be87e254adaf24857bccb84449838dd95e460bc7
Salonirana27 <sr2806751@gmail.com>
2026-02-18 23:57:09 +0530

    python lab programs

---
+++ control_statements/student_distribution.py
@@ -0,0 +1,8 @@
+total_students = 125
+students_per_class = 30
+
+full_classes = total_students // students_per_class
+remaining_students = total_students % students_per_class
+
+print("Full Classes =", full_classes)
+print("Remaining Students =", remaining_students)
