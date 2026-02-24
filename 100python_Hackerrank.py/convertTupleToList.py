52a68818d7a4bb26319e012b458a64d33c3bfd0c
Salonirana27 <sr2806751@gmail.com>
2026-02-24 15:29:26 +0530

    added program 68

---
+++ 100python_Hackerrank.py/convertTupleToList.py
@@ -0,0 +1,16 @@
+#Convert tuple to list.
+
+
+def tuple_to_list(tpl):
+    return list(tpl)
+
+# Taking input
+numbers = tuple(map(int, input("Enter tuple elements: ").split()))
+
+# Converting and printing result
+converted_list = tuple_to_list(numbers)
+print("Converted list is:", converted_list)
+
+# output
+ """ Enter tuple elements: 1 2 3 4 5
+  Converted list is: [1, 2, 3, 4, 5] """
