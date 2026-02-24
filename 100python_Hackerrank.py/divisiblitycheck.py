783551572b9b27962c79a4bbf09890def40800c2
Salonirana27 <sr2806751@gmail.com>
2026-02-24 11:38:07 +0530

    added program 15

---
+++ 100python_Hackerrank.py/divisiblitycheck.py
@@ -0,0 +1,16 @@
+#Check whether a number is divisible by both 3 and 5
+num = int(input("Enter the number: "))
+
+if num % 3 == 0 and num % 5 == 0:
+    print("Divisible by both 3 and 5")
+else:
+    print("Not divisible by both 3 and 5")
+    
+    #output
+    """Enter the number: 15
+Divisible by both 3 and 5
+
+nter the number: 15
+Divisible by both 3 and 5s
+
+"""
