266b79228435e5e619372e1b743c011a6b26b99a
Salonirana27 <sr2806751@gmail.com>
2026-02-24 14:23:25 +0530

    added program 44

---
+++ 100python_Hackerrank.py/maxOfThreeNumberUsingFunction.py
@@ -0,0 +1,20 @@
+#find Maximum of three numbers using function
+# Function to find maximum
+def find_max(a, b, c):
+    return max(a, b, c)
+
+# Read input
+a = int(input("enter number1: "))
+b = int(input("enter number1:"))
+c = int(input("enter number1: "))
+
+# Print result
+print("maximum number is : ")
+print(find_max(a, b, c))
+
+#output
+"""enter number1: 56
+enter number1:59
+enter number1: 78
+maximum number is : 
+78"""
